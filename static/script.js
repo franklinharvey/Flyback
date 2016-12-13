3010ab33685cc5f59a987e02cc69676288375850
Franklin Harvey <Franklinadamsharvey@gmail.com>
2016-12-13 15:13:01 -0700

    Create HTML and Firebase

---
+++ static/script.js
@@ -0,0 +1,12 @@
+(function() {
+	var config = {
+		apiKey: "AIzaSyAPpCQEyr_E8seQdF75zSrWKaACM2bPr_0",
+	    authDomain: "watchapp-992f5.firebaseapp.com",
+	    databaseURL: "https://watchapp-992f5.firebaseio.com",
+	    storageBucket: "watchapp-992f5.appspot.com",
+	    messagingSenderId: "836981217641"
+	};
+	firebase.initializeApp(config);
+	console.log("hi");
+
+}());
