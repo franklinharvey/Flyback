9265ad602329508916e5a6a71aa99f6d4282de54
Franklin Harvey <Franklinadamsharvey@gmail.com>
2016-12-14 13:14:19 -0700

    change to flyback, start content loading

--- static/script.js
+++ static/script.js
@@ -7,6 +7,43 @@
 	    messagingSenderId: "836981217641"
 	};
 	firebase.initializeApp(config);
-	console.log("hi");
 
+
+	contentString = '<div class="content-window">' +
+			'<img src=' + imgSource + '>' + 
+			'<h3> ' + object + ' </h3>' +
+			'</div>';
+
+	object = "";
+	populate(object);
+
+	function loadPage(){
+		const content = document.getElementById("content");
+		
+	};
+
+	function populate(object){
+		var query = firebase.database().ref("Brands/" + object).orderByKey();
+		query.once("value")
+	  		.then(function(snapshot) {
+	    		snapshot.forEach(function(childSnapshot) {
+		      		// key will be "ada" the first time and "alan" the second time
+		      		var key = childSnapshot.key;
+		      		// childData will be the actual contents of the child
+		      		var childData = childSnapshot.val().RefNumber;
+		      		if (!childData){
+		      			console.log(key);
+		      		}
+		      		else {
+		      			console.log(key + " " + childData);
+		      		}
+	  			});
+			});
+  	}
+
+	function stripPunctuation (replaceString) {
+		replaceString = replaceString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+		replaceString = replaceString.replace(/\s{2,}/g," ");
+		return replaceString;
+	};
 }());
