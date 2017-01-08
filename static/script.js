b6a43b6472475dda1ef20157166cde33f96e8fff
Franklin Harvey <Franklinadamsharvey@gmail.com>
2017-01-07 20:50:06 -0700

    more content window

--- static/script.js
+++ static/script.js
@@ -26,9 +26,7 @@
 		query.once("value")
 	  		.then(function(snapshot) {
 	    		snapshot.forEach(function(childSnapshot) {
-		      		// key will be "ada" the first time and "alan" the second time
 		      		var key = childSnapshot.key;
-		      		// childData will be the actual contents of the child
 		      		var childData = childSnapshot.val().RefNumber;
 		      		if (!childData){
 		      			loadPage(key);
